c6320032be5cf03de44cfc55a413cd9a5a8f2312
Chad Johnston <cjohnston@megatome.com>
2022-12-29 18:26:56 -0700

    Add About page
    Closes #4

---
+++ libs/solver-ui/src/lib/about/about.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { AboutDialogComponent } from './dialog/about-dialog.component';
+
+@Component({
+  selector: 'ng-squaredle-solver-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss'],
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule, MatIconModule],
+})
+export class AboutComponent {
+  constructor(private readonly dialog: MatDialog) {}
+
+  public showAbout(): void {
+    this.dialog.open(AboutDialogComponent);
+  }
+}
